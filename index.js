432b6c82e6f0d033f2b5d2ddb8059351cfbf56f6
michael youssef <michaelyoussef396@gmail.com>
2023-12-02 13:02:07 +1100

    bird2 class still needs fixing

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+class Cat{
+    constructor(name, gender){
+        this.name = name;
+        this.gender = gender;
+    }
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog{
+    constructor(name, gender){
+        this.name = name;
+        this. gender = gender;
+    }
+
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    speak() {
+        return `It's me! ${this.name}, the parrot!`;
+    }
+}
+
+class bird2 {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    speak() {
+        return `${this.name} says squawk!`;
+    }
+}
